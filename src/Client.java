96e7c20a75ab6d5ae427df0ab626c0034ce741ab
Сергей Караульных <karaulnih@gmail.com>
2022-03-27 23:33:23 +0500

    Homework_2

--- src/Client.java
+++ src/Client.java
@@ -3,14 +3,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Client {
-    public static final String HOST = "localhost"; // задаем локальный хост
+    public static final String HOST = "netology.homework"; // задаем локальный хост
     private static final int PORT = 8080; // задаем номер порта для подключения к серверу
     private static Socket clientSocket; // сокет для общения
     private static PrintWriter out; // поток для записи в сокет
     private static BufferedReader in; // поток для чтения из сокета
+    private static int messages = Server.messages; // количество сообщений
 
     public static void main(String[] args) {
         try {
@@ -20,12 +23,16 @@ public class Client {
                         getOutputStream(), true); // создаем поток вывода для отправки сообщений
                 in = new BufferedReader(new InputStreamReader(clientSocket.
                         getInputStream())); // создаем поток ввода для приема сообщений
-                System.out.println("Введите Ваше имя:");
                 Scanner scanner = new Scanner(System.in);
-                String name = scanner.nextLine(); // читаем имя из консоли
-                out.println(name); // отправляем сообщение на сервер
-                String serverMessage = in.readLine(); // читаем сообщение с сервера
-                System.out.println("Сервер сообщает:" + serverMessage);
+                for (int i = 0 ; i < messages ; i++) {
+                    String serverMessage = in.readLine(); // читаем сообщение с сервера
+                    System.out.println("Сервер сообщает:\n" + serverMessage);
+                    if (i < 2) {
+                        System.out.println("Наш ответ:");
+                        String clientMessage = scanner.nextLine(); // читаем имя из консоли
+                        out.println(clientMessage); // отправляем сообщение на сервер
+                    }
+                }
             } finally {
                 clientSocket.close(); // закрываем сокет
                 in.close(); // закрываем поток ввода
--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Server {
     private static final int PORT = 8080; // задаем номер порта для подключения сервера
@@ -8,6 +11,14 @@ public class Server {
     private static Socket clientSocket; // сокет для общения
     private static PrintWriter out; // поток для записи в сокет
     private static BufferedReader in; // поток для чтения из сокета
+    private static String clientName; // переменная для записи имени клиента
+    private static List<String> serverMessages = new ArrayList<>(Arrays.asList( // список фраз сервера
+            "Write your name!",
+            "Are you child (yes/no)?",
+            "Welcome to the kids area, %username%! Let's play!",
+            "Welcome to the adult zone, %username%! Have a good rest, or a good working day!"));
+    static int messages = serverMessages.size() - 1; // количество сообщений
+    private static List<String> clientMessages = new ArrayList<>(); // список фраз клиента
 
     public static void main(String[] args) {
         try {
@@ -20,9 +31,29 @@ public class Server {
                     out = new PrintWriter(clientSocket.
                             getOutputStream(), true); // создаем поток вывода для отправки сообщений
                     System.out.println("New connection accepted"); // подтверждаем установление соединения
-                    String name = in.readLine(); // читаем сообщение от клиента
-                    out.println(String.format("Hi %s, your port is %d", name,
-                            clientSocket.getPort())); // выводми на экран сообщение и номер порта
+                    for (int i = 0 ; i < messages ; i++) {
+                        String serverMessage;
+                        String clientMessage;
+                        if (i > 1) {
+                            if ("no".equals(clientMessages.get(1))) {
+                                serverMessage = serverMessages.get(i + 1).replace("%username%", clientName);
+                            } else {
+                                serverMessage = serverMessages.get(i).replace("%username%", clientName);
+                            }
+                        } else {
+                            serverMessage = serverMessages.get(i);
+                        }
+                        out.println(serverMessage); // направляем сообщение клиенту
+                        System.out.println("Я (сервер) сообщил:\n" + serverMessage);
+                        if (i < 2) {
+                            clientMessage = in.readLine(); // читаем сообщение от клиента
+                            System.out.println("Клиент ответил: \n" + clientMessage);
+                            clientMessages.add(clientMessage); // сохраняем ответ клиента
+                            if (i == 0) {
+                                clientName = clientMessage; // сохраняем имя клиента
+                            }
+                        }
+                    }
                 } finally {
                     clientSocket.close(); // закрываем сокет
                     in.close(); // закрываем поток ввода
